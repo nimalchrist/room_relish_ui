8d64085cf831fc56ab188613a30eaec0271f4992
nimalchrist <selvanimal0@gmail.com>
2024-03-29 21:44:08 +0530

    all pages ui template commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,64 @@
+import Home from "./pages/Home";
+import "./assets/styles/Main.css";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./Layout";
+import HotelList from "./pages/HotelList";
+import HotelDetail from "./pages/HotelDetail";
+import BookingPage from "./pages/BookingPage";
+import PaymentsPage from "./pages/PaymentsPage";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        errorElement: <div>404 Not found</div>,
+        children: [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path: '/hotel-list',
+                element: <HotelList/>
+            },
+            {
+                path: '/hotel-list/:hotelId',
+                element: <HotelDetail/>
+            },
+            {
+                path: '/hotel-list/:hotelId/booking',
+                element: <BookingPage/>
+            },
+            {
+                path: '/hotel-list/:hotelId/booking/:roomId',
+                element: <PaymentsPage/>
+            },
+            {
+                path: '/login',
+                element: <HotelList/>
+            },
+            {
+                path: '/signup',
+                element: <HotelList/>
+            }
+        ]
+    }
+]);
+
 function Main() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+    return (
+        <div className="App">
+            <RouterProvider router={router}/>
+            {/*<BrowserRouter>*/}
+            {/*    <Routes>*/}
+            {/*        <Route path="/" element={<Layout/>}>*/}
+            {/*            <Route index element={<Home/>}/>*/}
+            {/*            <Route path="hotel-list" element={<HotelList/>}></Route>*/}
+            {/*        </Route>*/}
+            {/*    </Routes>*/}
+            {/*</BrowserRouter>*/}
+        </div>
+    );
 }
 
 export default Main;
