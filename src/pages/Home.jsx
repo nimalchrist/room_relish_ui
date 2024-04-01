1841f0ad9b53b521912c195259061562983a50de
nimalchrist <selvanimal0@gmail.com>
2024-04-01 16:38:34 +0530

    all pages ui static scenario completed t1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import HeroSection from "../components/hero-section/HeroSection";
 import "../assets/styles/Home.css";
 function Home() {
+
     return(
       <>
         <div className="home-page">
